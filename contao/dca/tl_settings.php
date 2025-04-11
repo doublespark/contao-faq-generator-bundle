b8de951eb110d975a3f7f34736651796abe81344
Jamie Devine <jamie.devine@doublespark.co.uk>
2025-04-11 11:24:50 +0100

    Add setting for AlsoAsked environment

--- contao/dca/tl_settings.php
+++ contao/dca/tl_settings.php
@@ -5,6 +5,7 @@ use Contao\CoreBundle\DataContainer\PaletteManipulator;
 PaletteManipulator::create()
     ->addLegend('faq_generator_legend', 'chmod_legend', PaletteManipulator::POSITION_BEFORE)
     ->addField('fg_alsoAskedApiKey', 'faq_generator_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('fg_alsoAskedApiEnv', 'faq_generator_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('fg_openAiApiKey', 'faq_generator_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('default', 'tl_settings')
 ;
@@ -12,6 +13,15 @@ PaletteManipulator::create()
 $GLOBALS['TL_DCA']['tl_settings']['fields']['fg_alsoAskedApiKey'] = [
     'inputType' => 'text',
     'eval'      => array('mandatory'=>false, 'tl_class'=>'w100')
+];
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['fg_alsoAskedApiEnv'] = [
+    'inputType' => 'select',
+    'options' => [
+        'sandbox' => 'Sandbox',
+        'production' => 'Production'
+    ],
+    'eval' => array('mandatory'=>true, 'tl_class'=>'w100')
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['fg_openAiApiKey'] = [
