ac23f57b4effaf7d8f4b28f61668b740b7e49ae9
Jamie Devine <jamie.devine@doublespark.co.uk>
2025-04-15 09:54:09 +0100

    Remove unused imports

--- src/Controller/BackendGenerateQuestionsController.php
+++ src/Controller/BackendGenerateQuestionsController.php
@@ -13,8 +13,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
-use Twig\Environment;
 
 class BackendGenerateQuestionsController {
 
