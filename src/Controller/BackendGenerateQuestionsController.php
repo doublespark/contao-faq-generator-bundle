1f82bf6ed2851fff46f6e155c05c3e7aedec1bb6
Jamie Devine <jamie.devine@doublespark.co.uk>
2025-04-16 12:32:50 +0100

    Remove unused imports

--- src/Controller/BackendGenerateQuestionsController.php
+++ src/Controller/BackendGenerateQuestionsController.php
@@ -9,8 +9,6 @@ use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\DataContainer;
 use Contao\Message;
 use Doctrine\DBAL\Connection;
-use Doublespark\FaqGeneratorBundle\Model\FaqQuestionModel;
-use Doublespark\FaqGeneratorBundle\Options\GenerationStatusOptions;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
